a7dbc4de54d73b647ba15e14b8b900525935eb08
Tarso Queiroz <tarsoqueiroz@gmail.com>
2018-05-03 16:58:19 -0300

    Udemy Go language course - fundamentals

---
+++ Udemy-Curso.Go/Controles/desafio/desafio.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func notaParaConceito(n float64) string {
+	switch {
+	case n >= 9 && n <= 10:
+		return "A"
+	case n >= 8 && n < 9:
+		return "B"
+	case n >= 5 && n < 8:
+		return "C"
+	case n >= 3 && n < 5:
+		return "D"
+	default:
+		return "E"
+	}
+}
+
+func main() {
+	fmt.Println(notaParaConceito(9.8))
+	fmt.Println(notaParaConceito(8.3))
+	fmt.Println(notaParaConceito(6.7))
+	fmt.Println(notaParaConceito(4.1))
+	fmt.Println(notaParaConceito(1.2))
+}
