c1f46fc645ee24186d03b34151ac0e22409568a8
Tarso Queiroz <tarsoqueiroz@gmail.com>
2018-05-14 14:49:34 -0300

    Go with MySQL

---
+++ Udemy-Curso.Go/SQL/estrutura/estrutura.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func exec(db *sql.DB, sql string) sql.Result {
+	result, err := db.Exec(sql)
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:changeit")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	exec(db, "create database if not exists cursogo")
+}
